2e4bea34641d635347d6e38f2a56da7afe4bffd2
smorency44 <smorency44@hotmail.com>
2026-01-10 23:27:55 -0600

    Update showcase items with new images

--- src/pages/showcase.js
+++ src/pages/showcase.js
@@ -4,10 +4,10 @@ import Link from '@docusaurus/Link';
 import styles from './showcase.module.css';
 
 const items = [
-  { title: 'Armoured Vehicles', desc: 'LAV family, tracked platforms, and supporting vehicles.', img: '/img/placeholder-armour.png', to: '/docs/showcase/armour' },
-  { title: 'Aircraft', desc: 'Rotary and fixed-wing platforms, from utility to strike.', img: '/img/placeholder-air.png', to: '/docs/showcase/air' },
-  { title: 'Weapons & Systems', desc: 'Weapon packs, sensors, and supporting systems.', img: '/img/placeholder-weapons.png', to: '/docs/showcase/weapons' },
-  { title: 'Gear & Uniforms', desc: 'CAF-inspired equipment, uniforms, and loadouts.', img: '/img/placeholder-gear.png', to: '/docs/showcase/gear' },
+  { title: 'Armoured Vehicles', desc: 'LAV family, tracked platforms, and supporting vehicles.', img: '/img/tfc_ca_leo24m_tw_ca.png', to: '/docs/showcase/armour' },
+  { title: 'Aircraft', desc: 'Rotary and fixed-wing platforms, from utility to strike.', img: '/img/tfc_air_cc130_ca.png', to: '/docs/showcase/air' },
+  { title: 'Weapons & Systems', desc: 'Weapon packs, sensors, and supporting systems.', img: '/img/tfc_w_c7a2_m203_x_ca.png', to: '/docs/showcase/weapons' },
+  { title: 'Gear & Uniforms', desc: 'CAF-inspired equipment, uniforms, and loadouts.', img: '/img/tfc_soldier_army_c7_mt_ca.png', to: '/docs/showcase/gear' },
 ];
 
 function Card({title, desc, img, to}) {
